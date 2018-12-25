3e503de7fd4c4cd6b3e0c63311e6749af0c1d7c0
Fatuma Ismail <timaismail90gmail.com>
2018-12-25 13:40:38 -0600

    Adds ingredients exercise

---
+++ ingredients.js
@@ -0,0 +1,24 @@
+var ingredients = ["eggs", "milk", "flour", "baking soda", "baking powder", "chocolate chips", "bananas"];
+console.log("ingredients to buy:");
+for (var i = 0; i < ingredients.length; i++){
+	console.log(ingredients[i]);
+}
+
+var ingredients = ["eggs", "milk", "flour", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var i = 0
+while (i < ingredients.length){
+	console.log(ingredients);
+	i++
+}
+console.log("ingredients to buy");
+
+var ingredients = ["eggs", "milk", "flour", "baking soda", "baking powder", "chocolate chips", "bananas"];
+console.log("ingredients to buy:");
+for (var i = 0; i < ingredients.length; i++){
+	console.log(ingredients[i]);
+
+}
+
+for(var i = ingredients.length-1;i>=0;i--){
+	console.log(ingredients[i]);
+}

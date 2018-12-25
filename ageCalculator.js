94949ec751f3c36369fc4d2b427b75a25132197a
Fatuma Ismail <timaismail90gmail.com>
2018-12-25 12:37:32 -0600

    Age calculator function

---
+++ ageCalculator.js
@@ -0,0 +1,8 @@
+function ageCalculator(name, yearOfBirth, currentYear){
+	return name + "is" + currentYear - yearOfBirth;
+
+var name = "suzie";
+var yearOfBirth = 1983;
+var currentYear = 2015;
+}
+

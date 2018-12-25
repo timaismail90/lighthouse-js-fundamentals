b2356c5fbf3be56eb133574b9b591454de6f8c51
Fatuma Ismail <timaismail90gmail.com>
2018-12-25 11:22:28 -0600

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,35 @@
+var raining = true;
+var cold = false;
+
+if (raining) {
+	console.log("Don't forget umbrella");
+}
+if(cold){
+	console.log("make sure you pick out a scarf!");
+
+}
+
+console.log("Now you are ready to go outside!")
+
+var cold = false;
+
+if(cold){
+	console.log("Make sure you pick a scarf");
+} else {
+	console.log("Short sleeves are fine.");
+}
+var temperature = 12
+
+if (temperature<8) {
+	console.log("Make sure you pick a scarf");
+} else if(temperature< 15) {
+	console.log("short sleeves won't cut it!");
+} else {
+	console.log ("short sleeves are fine.");
+}
+
+console.log("Now you are ready to go outside!");
+
+if (isCitizen && age > 18) {
+	console.log("you are eligible to vote");
+}
